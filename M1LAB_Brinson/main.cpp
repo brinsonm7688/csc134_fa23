2ef79cf070d1334191b98f2cb115e61277852665
brinsonm7688 <143422868+brinsonm7688@users.noreply.github.com>
2023-08-28 11:25:51 -0400

    Started m1LAB

---
+++ M1LAB_Brinson/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+CSC 134
+M1LAB - Apple Orchard
+brinson
+8/28
+Simple interactive store.
+*/
+
+
+int main()
+{
+    string name = "Brinson";
+    cout << "Welcome to the " << name << " orchard." << endl;
+    return 0;
+}
